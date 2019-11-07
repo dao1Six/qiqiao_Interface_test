29a038db07a14988ec1310a01be5f06741476933
wujianlun <905734003@qq.com>
2019-11-07 23:51:46 +0800

    补充UI自动化

--- page_obj/PC/components/address_component.py
+++ page_obj/PC/components/address_component.py
@@ -5,10 +5,9 @@ from page_obj.selenium_page import SeleniumPage
 class Address(SeleniumPage):
 
     #输入地址组件的值
-    def sendkeysToAddress(self,fieldName,addkey,detilkey):
-        elems  = self.find_elemsByCSS("div[title='"+fieldName+"'] input")
-        self.driver.execute_script ("arguments[0].removeAttribute('readonly');", elems)
-        elems[0].send_keys(addkey)
-        elems[1].send_keys(detilkey)
-
+    def sendkeysToAddress(self,fieldName,addkeys,detilkey):
+        self.clickElemByXpath_Visibility("//div[@title='"+fieldName+"']//span[@class='el-cascader__label']")
+        for i in addkeys:
+            self.clickElemByXpath_Visibility("//div[@class='el-cascader-menus el-popper']//span[text()='"+i+"']")
+        self.sendkeysElemByXpath_Visibility("//div[@title='"+fieldName+"']//input[@placeholder='详细地址']",detilkey)
     #给地址组件输入值
--- page_obj/PC/components/cascade_component.py
+++ page_obj/PC/components/cascade_component.py
@@ -5,9 +5,9 @@ from page_obj.selenium_page import SeleniumPage
 class Cascade(SeleniumPage):
 
     #输入级联组件的值
-    def sendkeysToCascade(self,fieldName,key):
-        elem  = self.find_elemByCSS("div[title='"+fieldName+"'] input")
-        self.driver.execute_script ("arguments[0].removeAttribute('readonly');", elem)
-        elem.send_keys(key)
+    def sendkeysToCascade(self,fieldName,keys):
+        self.clickElemByXpath_Visibility("//div[@title='"+fieldName+"']//span[@class='el-cascader__label']")
+        for i in keys:
+            self.clickElemByXpath_Visibility("//div[@class='el-cascader-menus el-popper']//span[contains(text(),'"+i+"')]")
 
     #给级联组件输入值
