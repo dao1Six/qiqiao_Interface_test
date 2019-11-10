bee28d66b82bf3bf0d54759c9a8a4eee54d28441
wujianlun <905734003@qq.com>
2019-11-10 14:13:01 +0800

    补充UI自动化

--- page_obj/PC/portal_page.py
+++ page_obj/PC/portal_page.py
@@ -7,13 +7,14 @@ class PortalPage(SeleniumPage):
     '''工作台页面'''
 
     #Action
+    header_menu_title_loc = "//a[@class='header_menu_title' and text()='%menu']"
 
 
 
     #点击菜单进入相应菜单页面
     def goto_menuPage(self,menu):
 
-        self.clickElemByXpath_Visibility("//a[@class='header_menu_title' and text()='"+menu+"']")
+        self.clickElemByXpath_Visibility(self.header_menu_title_loc.replace('%menu',menu))
 
 
 
