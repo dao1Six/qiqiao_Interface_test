1e20d33da04912cc739a62c215eb17dd6c87cc5c
wujianlun <wujianlun@do1.com.cn>
2019-11-11 16:49:24 +0800

    补充UI自动化

--- page_obj/PC/components/user_component.py
+++ page_obj/PC/components/user_component.py
@@ -6,21 +6,14 @@ from page_obj.selenium_page import SeleniumPage
 
 class User(SeleniumPage):
 
-    UserquerenButton = "//div[@class='v-modal']//following-sibling::div/div[@aria-label='人员列表']//div[@class='dialog-footer ovf-hd']/button[2]/span"
+    UserquerenButton = "//div[@class='v-modal']//following-sibling::div/div[@aria-label='人员列表']//div[@class='dialog-footer ovf-hd']/button[2]/span" #人员选择确认按钮
 
+    user_select_loc = "//div[@title='%s']//span[text()='+选择人员']"  #人员选择字段添加按钮
 
-    DeptquerenButton = "//div[@class='v-modal']//following-sibling::div/div[@aria-label='部门列表']//div[@class='dialog-footer ovf-hd']/button[2]/span"
+    user_search_loc = "//input[@placeholder='搜索用户']"  #人员选择组织架构搜索框
 
-    user_select_loc = "//div[@title='%s']//span[text()='+选择人员']"
+    user_option_loc = "//li/span[contains(text(),'%s')]"  #人员选择组织架构搜索项
 
-    dept_select_loc = "//div[@title='%s']//span[text()='+选择部门']"
-
-    user_search_loc = "//input[@placeholder='搜索用户']"
-    dept_search_loc = "//input[@placeholder='搜索部门']"
-
-    user_option_loc = "//li/span[contains(text(),'%s')]"
-
-    dept_option_loc = "//li/span[contains(text(),'%s')]"
 
 
     # 给人员单选组件输入值
@@ -43,23 +36,5 @@ class User(SeleniumPage):
         self.clickElemByXpath_Visibility(self.UserquerenButton)
 
 
-    # 给部门单选组件输入值
-    def sendkeysToMonomialDept(self,fieldName,DeptName):
-        #点击选择框
-        self.clickElemByXpath_Visibility(self.dept_select_loc.replace('%s',fieldName))
-        self.sendkeysElemByXpath_Visibility(self.dept_search_loc,DeptName)
-        self.clickElemByXpath_Visibility(self.dept_option_loc.replace('%s',DeptName))
-        self.clickElemByXpath_Visibility(self.DeptquerenButton)
 
-
-    # 给部门多选组件输入值
-    def sendkeysToMultiDept(self,fieldName,DeptNameList):
-        #点击选择框
-        self.clickElemByXpath_Visibility(self.dept_select_loc.replace('%s',fieldName))
-        for name in DeptNameList:
-            self.clickElemByXpath_Visibility (self.dept_search_loc)
-            self.sendkeysElemByXpath_Visibility(self.dept_search_loc,name)
-            e = self.find_elemByXPATH(self.dept_option_loc.replace('%s',name))
-            e.click()
-        self.clickElemByXpath_Visibility(self.DeptquerenButton)
 
